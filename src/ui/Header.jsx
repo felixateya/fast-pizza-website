17fd488a49babbcfb6fd53cade585e3c307d321b
felixateya <mulaafelix6@gmail.com>
2023-11-09 09:03:56 +0300

    issue handling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/UserName";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
@@ -9,7 +9,7 @@ function Header() {
         Fast React Pizza Co.
       </Link>
       <SearchOrder />
-      <Username />
+      <UserName />
     </header>
   );
 }
