012be78adf67762053ab46647f35aca7dc0dedb7
felixateya <mulaafelix6@gmail.com>
2023-11-09 08:57:22 +0300

    first commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+// import SearchOrder from "../features/order/SearchOrder";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      
+      <Header />
+      {/* <SearchOrder /> */}
+      <div className=" overflow-scroll">
+        <main className=" max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
