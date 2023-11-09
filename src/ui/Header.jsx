cfd81bbc05b01934920e1e82fb4aa06a1df8a732
felixateya <mulaafelix6@gmail.com>
2023-11-09 11:24:25 +0300

    new alterations

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex flex-col items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+    <header className="flex flex-col sm:flex-row items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest mb-3">
         Fast React Pizza Co.
       </Link>
